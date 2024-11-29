8507d472ac56e72d4ba1d5278c7f983ea420addf
Allyson Costa <allyson.wrk2024@gmail.com>
2024-11-29 01:40:17 -0300

    add JsonView e métodos para atualziar usuário, senha e imagem

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,13 +1,16 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.dtos.UserRecordDTO;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -31,5 +34,31 @@ public class UserController {
     public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
         userService.delete(userService.findById(userId).get());
         return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "userId") UUID userId,
+                                             @RequestBody @JsonView(UserRecordDTO.UserView.UserPut.class) UserRecordDTO userRecordDTO){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(userRecordDTO, userService.findById(userId).get()));
+    }
+
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<Object> updatePassword(@PathVariable(value = "userId") UUID userId,
+                                             @RequestBody @JsonView(UserRecordDTO.UserView.PasswordPut.class) UserRecordDTO userRecordDTO){
+
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if(!userModelOptional.get().getPassword().equals(userRecordDTO.oldPassword())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Passwords do not match");
+        }
+        userService.updatePassword(userRecordDTO, userModelOptional.get());
+
+        return ResponseEntity.status(HttpStatus.OK).body("Password updated successfully");
+    }
+
+    @PutMapping("/{userId}/image")
+    public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId,
+                                             @RequestBody @JsonView(UserRecordDTO.UserView.ImagePut.class) UserRecordDTO userRecordDTO){
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateImage(userRecordDTO, userService.findById(userId).get()));
     }
 }
