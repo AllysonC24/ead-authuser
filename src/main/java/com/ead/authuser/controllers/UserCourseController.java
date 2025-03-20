210c546e44fe01959542fc17030385d1d21405cd
Allyson Costa <allyson.wrk2024@gmail.com>
2025-03-20 15:19:02 -0300

    feat: adicionado inscrição de user in course usando comunicação síncrona

--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -2,6 +2,12 @@ package com.ead.authuser.controllers;
 
 import com.ead.authuser.clients.CourseClient;
 import com.ead.authuser.dtos.CourseRecordDTO;
+import com.ead.authuser.dtos.UserCourseRecordDTO;
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserCourseService;
+import com.ead.authuser.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,10 +15,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -20,10 +25,30 @@ import java.util.UUID;
 public class UserCourseController {
 
     final CourseClient courseClient;
+    final UserService userService;
+    final UserCourseService userCourseService;
 
     @GetMapping("/users/{userId}/courses")
      public ResponseEntity<Page<CourseRecordDTO>> getAllCoursesByUser(@PageableDefault(sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
                                                      @PathVariable(value = "userId") UUID userId){
         return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(pageable, userId));
     }
+
+    @PostMapping("/users/{userId}/courses/subscription")
+    public ResponseEntity<Object> saveSubscriptionUserInCourse(@PathVariable(value = "userId") UUID userId,
+                                                               @RequestBody @Valid UserCourseRecordDTO userCourseRecordDTO){
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if(userCourseService.existsByUserAndCourseId(userModelOptional.get(), userCourseRecordDTO.courseId())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Subscription already exists");
+        }
+
+        UserCourseModel userCourseModel = userCourseService.saveSubscription(
+                userModelOptional.get().convertToUserCourseModel(userCourseRecordDTO.courseId())
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCourseModel);
+    }
+
+
+
 }
